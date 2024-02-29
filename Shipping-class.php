7ad3b160ea37223bce5aea74c964570b15db27d4
VladimirSark <143695123+VladimirSark@users.noreply.github.com>
2024-02-29 12:24:53 +0200

    Add files via upload

---
+++ Shipping-class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Display Shipping Class
+Description: Display shipping class in product page
+Version: 1.0.0
+Author: Vladimir
+Author URI: SlapiosNosys.lt
+*/
+
+/* Display the shipping class on the product page.
+*/
+function show_shipping_class_on_product_page() {
+    global $product;
+
+    $shipping_class_id = $product->get_shipping_class_id();
+
+    if ($shipping_class_id > 0) {
+        $shipping_class = get_term($shipping_class_id, 'product_shipping_class');
+        $shipping_class_name = $shipping_class->name;
+        echo '<p class="shipping-class">Sandėliuojama: ' . $shipping_class_name . '</p>';
+    }
+}
+add_action('woocommerce_single_product_summary', 'show_shipping_class_on_product_page', 25);
